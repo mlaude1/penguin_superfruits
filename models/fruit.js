28c038dd343c59a0b81543531235f48d5d2a8969
Matthew Laude <laude_matthew@yahoo.com>
2021-10-25 16:15:01 -0400

    superfruits updated

---
+++ models/fruit.js
@@ -0,0 +1,31 @@
+////////////////////////
+// import dependencies
+////////////////////////
+// import the existing connected mongoose object from connection.js
+const mongoose = require("./connection")
+
+
+////////////////////////////////////////////////
+// Create our Fruits Model
+////////////////////////////////////////////////
+// pull schema and model from mongoose
+const {Schema, model} = mongoose // destructuring Schema and Model from mongoose
+
+// make a fruits schema
+const fruitsSchema = new Schema({
+    name: String,
+    color: String,
+    readyToEat: Boolean,
+});
+
+// Make the Fruit Model
+const Fruit = model("Fruit", fruitsSchema) // the model should always be capitalized and singular
+
+// log the model to make sure it exists
+// console.log(Fruit)
+
+////////////////////////
+// export the fruit model
+////////////////////////
+
+module.exports = Fruit
